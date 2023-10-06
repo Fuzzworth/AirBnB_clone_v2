819b707b046d90cb681274fa4c6d76587aef0a15
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-10-06 02:46:34 +0200

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -14,7 +14,7 @@ def do_pack():
     Function Docs
     """
     file = "versions/web_static_{}.tgz".format(
-                datetime.now().strftime('%Y%m%d%H%M%S')
+            datetime.now().strftime('%Y%m%d%H%M%S')
             )
     print(f"Packing web_static to {file}")
     if local(f"mkdir -p versions && tar -cvzf {file} web_static").succeeded:
@@ -31,16 +31,20 @@ def do_deploy(archive_path):
             return False
         ext = basename(archive_path)
         no_ext, ext = splitext(ext)
-        dpath = "/data/web_static/releases/"
+        web_static_dir = "/data/web_static/releases/"
         put(archive_path, "/tmp/")
-        run("rm -rf {}{}/".format(dpath, fn_no_ext))
-        run("mkdir -p {}{}/".format(dpath, fn_no_ext))
-        run("tar -xzf /tmp/{} -C {}{}/".format(fn_with_ext, dpath, fn_no_ext))
-        run("rm /tmp/{}".format(fn_with_ext))
-        run("mv {0}{1}/web_static/* {0}{1}/".format(dpath, fn_no_ext))
-        run("rm -rf {}{}/web_static".format(dpath, fn_no_ext))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {}{}/ /data/web_static/current".format(dpath, fn_no_ext))
+        commands = [
+                f"rm -rf {web_static_dir}{no_ext}/",
+                f"mkdir -p {web_static_dir}{no_ext}/",
+                f"tar -xzf /tmp/{ext} -C {web_static_dir}{no_ext}/",
+                f"rm /tmp/{ext}",
+                f"mv {web_static_dir}{no_ext}/web_static/* {web_static_dir}{no_ext}/",
+                f"rm -rf {web_static_dir}{no_ext}/web_static",
+                f"rm -rf /data/web_static/current",
+                f"ln -s {web_static_dir}{no_ext}/ /data/web_static/current",
+                ]
+        for command in commands:
+            run(command)
         print("New version deployed!")
         return True
     except Exception:
