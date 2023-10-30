4d5528bd530dd6a7e3057ac8f11f26c4ec47ffa3
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2023-10-30 12:15:55 +0200

    Task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Module Docs
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def task0():
+    """
+    Function Docs
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def task1():
+    """
+    Function Docs
+    """
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """
+    Function Docs
+    """
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(port='5000', host='0.0.0.0')
